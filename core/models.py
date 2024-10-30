92ead3a67c038232884f9da5136468481b73455f
Andy <obrezkov.andy@gmail.com>
2024-10-30 18:29:52 +0300

    Create Member and Role models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
+    # image = models.ImageField(upload_to='members/')
+    role = models.ForeignKey('Role', on_delete=models.PROTECT, related_name='members')
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+    class Meta:
+        ordering = ['time_created']
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
